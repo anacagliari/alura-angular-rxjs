a2040fd2949a7b35ed3750a05299339f7bb69c40
Ana Cagliari <111251330+anacagliari@users.noreply.github.com>
2025-04-22 14:20:41 -0300

    alura-angular-rxjs-aula02

--- src/app/views/lista-livros/lista-livros.component.ts
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { LivroService } from 'src/app/service/livro.service';
 
 @Component({
@@ -6,18 +7,26 @@ import { LivroService } from 'src/app/service/livro.service';
   templateUrl: './lista-livros.component.html',
   styleUrls: ['./lista-livros.component.css']
 })
-export class ListaLivrosComponent {
+export class ListaLivrosComponent implements OnDestroy {
 
   listaLivros: [];
   campoBusca: string = '';
+  subscription: Subscription;
 
   constructor(private service: LivroService) { }
 
   buscarLivro() {
-    this.service.buscar(this.campoBusca).subscribe(
-      (returnoAPI) => console.log(returnoAPI),
-      (error) => console.error(error)
-      );
+    this.subscription = this.service.buscar(this.campoBusca).subscribe({
+      next: retornoAPI => console.log(retornoAPI),
+      error: erro => console.log(erro),
+      complete: () => console.log('Requisição "Observable" completa')
+    }
+    );
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+    console.log('Requisição "Observable" cancelada');
   }
 
 }
